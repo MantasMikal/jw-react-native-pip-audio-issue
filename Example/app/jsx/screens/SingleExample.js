d4924e59fd048c477765c01fc0de7eb70acc03dc
MantasMikal <mantas.codes@gmail.com>
2024-10-03 12:08:32 +0300

    reproduce pip audio issue

--- Example/app/jsx/screens/SingleExample.js
+++ Example/app/jsx/screens/SingleExample.js
@@ -1,16 +1,9 @@
 import React, {useRef} from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import Player from '../components/Player';
-import PlayerContainer from '../components/PlayerContainer';
 
 export default () => {
   const playerRef = useRef([]);
-
-  const onTime = e => {
-    // var {position, duration} = e.nativeEvent;
-    // eslint-disable-line
-    // console.log('onTime was called with: ', position, duration);
-  };
 
   const onFullScreen = () => {
     StatusBar.setHidden(true);
@@ -21,50 +14,32 @@ export default () => {
   };
 
   let jwConfig = {
-    "title": "Single Inline Linear Preroll",
-    "playlist": [
+    title: 'Single Audio',
+    playlist: [
       {
-        "title": "Single Inline Linear Preroll",
-        "file": "https://content.bitsontherun.com/videos/q1fx20VZ-52qL9xLP.mp4",
-        "adschedule": {
-          "adBreak1": {
-            "offset": "pre",
-            "ad": {
-              "source": "googima",
-              "tag": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
-            }
-          }
-        }
-      }
+        title: 'Single Audio',
+        file: 'https://download.samplelib.com/mp3/sample-15s.mp3',
+        // file: "https://content.bitsontherun.com/videos/q1fx20VZ-52qL9xLP.mp4",
+      },
     ],
-    "advertising": {
-      "client": "googima"
-    }
-  }
+  };
 
-  const renderPlayer = () => {
-    return (
+  return (
+    <View style={{ height: 150 }}>
       <Player
         ref={playerRef}
         style={{flex: 1}}
         config={{
           autostart: true,
+          pipEnabled: true,
           styling: {
             colors: {},
           },
-          ...jwConfig
+          ...jwConfig,
         }}
-        onTime={onTime}
         onFullScreen={onFullScreen}
         onFullScreenExit={onFullScreenExit}
       />
-    );
-  };
-
-  return (
-    <PlayerContainer
-      children={renderPlayer()}
-      text="Welcome to jwplayer-react-native"
-    />
+    </View>
   );
 };
